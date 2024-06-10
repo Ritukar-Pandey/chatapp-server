0e2342d404839899f790ddaf5a5ea852acea4a7d
Ritukar <ritukarpandey@gmail.com>
2024-06-11 02:38:09 +0530

    bug fix

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ app.use((req,res,next)=>{
   }
   res.header(
       "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Credentials",
       "Origin,X-Requested-With,Content-Type,Accept"
   );
   next();
