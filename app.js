3d0886c93ceb5245ed7ddd0bd0a90e71eb28c72c
Ritukar <ritukarpandey@gmail.com>
2024-06-11 02:42:21 +0530

    fix

--- app.js
+++ app.js
@@ -56,18 +56,14 @@ app.set("io", io);
 // Using Middlewares Here
 app.use(express.json());
 app.use(cookieParser());
-app.use((req,res,next)=>{
-  const origin = req.headers.origin;
-  if (process.env.allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Credentials",
-      "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-})
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
